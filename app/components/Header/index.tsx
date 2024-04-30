4f423baf31470110d60c2d28197adfa5f74526e8
GustavoCR021 <gustavoc1922@gmail.com>
2024-04-30 00:04:43 -0300

    feat: header

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -5,10 +5,19 @@ export default function Header() {
     <A>
       <hr className="hrA"/>
       <div className="section">
-        <p>Street <span>Shop</span></p>
-        <a href="/">O que está procurando</a>
-        <div className="user">
-          <a href="aaaaaaaaa"></a>
+        <div className="fis">
+          <p>Street <span>Shop</span></p>
+          <a href="/">O que está procurando</a>
+          <div className="user">
+            <a href="aaaaaaaaa"></a>
+          </div>
+        </div>
+        <div className="nav">
+          <a href="/">Todos os produtos</a>
+          <a href="/">Roupas</a>
+          <a href="/">Compra Segura</a>
+          <a href="/">Contato</a>
+          <a href="/">Perguntas frequentes</a>
         </div>
       </div>
       <hr className="hrB"/>
--- app/components/Header/styles.ts
+++ app/components/Header/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const A = styled.div `
   width: 100%;
-  height: 15vh;
+  height: 18vh;
   background: #0e0e10;
 
   .section {
@@ -11,16 +11,21 @@ export const A = styled.div `
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
 
-    p {
-      font-family: "Sedgwick Ave Display";
-      color: #fff;
-      font-size: 45px;
-
+    .fis {
+      display: flex;
+      p {
+        font-family: "Sedgwick Ave Display";    
+        color: #fff;
+        font-size: 40px;  
+      }
       span {
         color: var(--prim);
       }
     }
+
+
     
     .user {
       background: red;
@@ -38,5 +43,19 @@ export const A = styled.div `
     border: none;
     height: 7px;
     background: linear-gradient(90deg, rgba(0,206,235,1) 0%, rgba(142,0,167,1) 100%);
+  }
+
+  .nav {
+    width: 100%;
+    height: 15%;
+    background: red;
+    display: flex;
+    justify-content: center;
+    gap: 3em;
+
+    a {
+      font-style: none;
+    }
+
   }
 `
