82ab75a652c686d83fcb5bae3e69aabfea7b1146
GustavoCR021 <gustavoc1922@gmail.com>
2024-05-07 15:47:39 -0300

    at

--- app/calcas/page.tsx
+++ app/calcas/page.tsx
@@ -13,23 +13,22 @@ export default function Test () {
 
   return (
     <>
-    <Header/>
-    <Car>
-      <h1 className="ti">Calças</h1>
-      <div className="cards">
-        {produtos
-        .filter(item => item.type === "calça")
-        .map(item => (
-          // eslint-disable-next-line react/jsx-key
-          <div className="card">
-            <h1 key={item.name}>{item.name}</h1>
-            <h2 key={item.price}>{item.price}</h2>
-            <Image src={item.photo} alt="test" width={235} height={280} style={{backgroundSize: "auto", backgroundRepeat: "no-repeat"}}/>
-          </div>
-        ))}
-      </div>
-    </Car>
-
+      <Header/>
+      <Car>
+        <h1 className="ti">Calças</h1>
+        <div className="cards">
+          {produtos
+          .filter(item => item.type === "calça")
+          .map(item => (
+            // eslint-disable-next-line react/jsx-key
+            <div className="card">
+              <h1 key={item.name}>{item.name}</h1>
+              <h2 key={item.price}>{item.price}</h2>
+              <Image src={item.photo} alt="test" width={235} height={280} style={{backgroundSize: "auto", backgroundRepeat: "no-repeat"}}/>
+            </div>
+          ))}
+        </div>
+      </Car>
     </>
   )
 }
