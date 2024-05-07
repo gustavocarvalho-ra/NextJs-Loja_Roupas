51924f75ad1da663dfd9c50f2dd49224e626031d
GustavoCR021 <gustavoc1922@gmail.com>
2024-05-07 13:36:31 -0300

    feat: json rename key

--- app/calcas/page.tsx
+++ app/calcas/page.tsx
@@ -19,7 +19,7 @@ export default function Test () {
         {produtos.map(item => (
           // eslint-disable-next-line react/jsx-key
           <div className="card">
-            <h1 key={item.nome}>{item.nome}</h1>
+            <h1 key={item.name}>{item.name}</h1>
             <h2 key={item.preco}>{item.preco}</h2>
             <Image src={item.imagem} alt="test" width={100} height={100} style={{background: "image"}}/>
           </div>
