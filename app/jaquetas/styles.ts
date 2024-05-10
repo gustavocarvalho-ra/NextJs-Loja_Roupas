ed8f150939fd880b3409badd4729cccd54f7815e
GustavoCR021 <gustavoc1922@gmail.com>
2024-05-09 23:53:09 -0300

    style: card background

--- app/jaquetas/styles.ts
+++ app/jaquetas/styles.ts
@@ -21,13 +21,15 @@ export const Jaq = styled.div `
   }
 
   .card {
-    width: 200px;
-    height: 290px;
+    width: 210px;
+    height: 300px;
     display: flex;
     align-items: center;
     flex-direction: column;
     transition: 1s;
     border-radius: 8px;
+    background-color: #f0f4f5;
+    padding-top: 1em;
     cursor: pointer;
 
     &:hover {
