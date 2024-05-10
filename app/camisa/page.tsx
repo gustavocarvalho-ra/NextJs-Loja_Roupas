bea0e55ffbac02024ba131f6d5a0a577ee965c76
GustavoCR021 <gustavoc1922@gmail.com>
2024-05-10 00:15:59 -0300

    feat: add page and styles of camisa

---
+++ app/camisa/page.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import pro from "../data/pro.json"
+import Header from "@/app/components/Header"
+import Menu from "../components/Menu";
+
+import Image from "next/image";
+import { Cam } from "./style";
+
+
+export default function Test () {
+  let produtos = [...pro];
+
+  return (
+    <>
+      <Header/>
+      
+      <Cam>
+        <Menu/>
+        
+        <div className="cards">
+          <div style={{width: "100%", height: "50px", marginBottom: "1em"}}>
+            <h1 className="ti">Jaquetas</h1>
+          </div>
+
+          {produtos
+          .filter(item => item.type === "camisa")
+          .map(item => (
+            // eslint-disable-next-line react/jsx-key
+            <div className="card">
+              <Image src={item.photo} alt="test" width={180} height={210} style={{backgroundSize: "auto", borderRadius: "8px"}}/>
+              <div className="info">
+                <h1 key={item.name}>{item.name}</h1>
+                <p key={item.price}>{item.price}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </Cam>
+    </>
+  )
+}
