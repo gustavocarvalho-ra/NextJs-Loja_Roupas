87bf4021a406c38cae691a58d8d80ea12450d016
GustavoCR021 <gustavoc1922@gmail.com>
2024-05-11 00:31:31 -0300

    style: menu color

--- app/components/Menu/index.tsx
+++ app/components/Menu/index.tsx
@@ -7,6 +7,7 @@ export default function Menu() {
   return (
     <Mn>
       <div className="menu">
+        <h1 style={{display: "flex", justifyContent: "center"}}>Menu Principal</h1>
         <Link href={"/all"}>Todos os Produtos</Link>
       </div>
     </Mn>
--- app/components/Menu/styles.ts
+++ app/components/Menu/styles.ts
@@ -9,5 +9,24 @@ export const Mn = styled.div `
     width: 100%;
     height: 300px;
     /* background: red; */
+    /* border: 1px solid #BA55D3; */
+    gap: 2em;
+    box-shadow: 4px 3px 5px #d3d3d3;
+
+    h1 {
+      font-size: 22px;
+      background-image: linear-gradient(90deg, rgba(0,206,235,1) 0%, rgba(142,0,167,1) 100%);
+      background-clip: text;
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      color: black;
+      margin: .5em 0 1em 0;
+    }
+
+    a {
+      text-decoration: none;
+      color: #BA55D3;
+      padding-left: 1em;
+    }
   }
 `
