6e53ea9c00edd624681389d0e7b2c8a8e04899c0
GustavoCR021 <gustavoc1922@gmail.com>
2024-05-12 22:45:00 -0300

    at link

--- app/components/Menu/index.tsx
+++ app/components/Menu/index.tsx
@@ -9,8 +9,8 @@ export default function Menu() {
       <div className="menu">
         <h1 style={{display: "flex", justifyContent: "center"}}>Menu Principal</h1>
         <Link href={"../pages/all"}>Todos os Produtos</Link>
-        <Link href={"../pages/all"}>Todos os Produtos</Link>
-        <Link href={"../pages/all"}>Todos os Produtos</Link>
+        <Link href={"../pages/jaquetas"}>Jaquetas</Link>
+        <Link href={"../pages/camisa"}>Camisetas</Link>
         <Link href={"../pages/all"}>Todos os Produtos</Link>
         <Link href={"../pages/all"}>Todos os Produtos</Link>
       </div>
