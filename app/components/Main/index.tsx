bf974521d27120fbc802267df589ce390db89575
GustavoCR021 <gustavoc1922@gmail.com>
2024-05-14 00:35:45 -0300

    at link

--- app/components/Main/index.tsx
+++ app/components/Main/index.tsx
@@ -14,7 +14,7 @@ export default function Main() {
           <Link className="cas" href="/pages/calcas">Calças</Link>
           <Link className="tes" href="/pages/tenis">Tênis</Link>
           <Link className="bon" href="/pages/bones">Bonés e Buckets</Link>
-          <a href="">Meias</a>
+          <Link className="bon" href="/pages/meias">Meias</Link>
           <a href="">Mochilas</a>
           <a href="">Óculos</a>
           <a href="">Bags</a>
