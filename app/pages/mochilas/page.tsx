0e6b5a612d74e31a96c440b5a6de99618031e2d4
GustavoCR021 <gustavoc1922@gmail.com>
2024-05-17 00:09:46 -0300

    add structure of page

---
+++ app/pages/mochilas/page.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import { Moc } from "./styles";
+
+import pro from "../../data/pro.json"
+import Header from "@/app/components/Header"
+import Menu from "../../components/Menu";
+
+import { products } from "@/app/types/products";
+
+import Image from "next/image";
+
+export default function Test () {
+  let produtos = [...pro];
+  let products = produtos
+
+  // var desconto = produtos.price * .3
+
+  return (
+    <>
+      <Header/>
+      
+      <Moc>
+        <Menu/>
+        
+        <div className="cards">
+          <div style={{width: "100%", height: "50px", marginBottom: "1em"}}>
+            <h1 className="ti">Bags</h1>
+          </div>
+
+          {produtos
+          .filter(item => item.type === "mochila")
+          .map(item => (
+            // eslint-disable-next-line react/jsx-key
+            <div className="card">
+                <button className="btd">25% off</button>
+              <div className="ft">
+                <Image src={item.photo}  alt="test" sizes="100vh" width={180} height={210} style={{borderRadius: "8px"}}/>
+              </div>
+              <div className="info">
+                <h1 key={item.name}>{item.name}</h1>
+                <div className="nis" style={{display: "flex"}}>
+                  <p key={item.price}>R$ {item.price}</p>
+                  <span key={item.price}>R$ 200</span>
+                  
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </Moc>
+    </>
+  )
+}
