7dd5a0231009184e5df4535891a91ff4ad0564f2
GustavoCR021 <gustavoc1922@gmail.com>
2024-05-18 23:25:49 -0300

    add structure

---
+++ app/pages/ace/page.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import pro from "../../data/pro.json"
+import Header from "@/app/components/Header"
+import Menu from "../../components/Menu";
+
+import Image from "next/image";
+import { Ace } from "./styles";
+
+
+export default function Test () {
+  let produtos = [...pro];
+
+  return (
+    <>
+      <Header/>
+      
+      <Ace>
+        <Menu/>
+        
+        <div className="cards">
+          <div style={{width: "100%", height: "50px", marginBottom: "1em"}}>
+            <h1 className="ti">Acessórios</h1>
+          </div>
+
+          {produtos
+          .sort(() => Math.random() - 0.5)
+          .map(item => (
+            // eslint-disable-next-line react/jsx-key
+            <div className="card">
+                <button className="btd">20% off</button>
+              <div className="ft">
+                <Image src={item.photo}  alt="test" sizes="100vh" width={180} height={210} style={{borderRadius: "8px", marginTop: "4px"}}/>
+              </div>
+              <div className="info">
+                <h1 key={item.name}>{item.name}</h1>
+              <div className="nis" style={{display: "flex", justifyContent: "space-between"}}>
+                  <p key={item.price}>R$ {item.price}</p>
+                  <span className="var" key={item.des}>R$ {Math.floor(item.price * item.des + item.price)}</span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </Ace>
+    </>
+  )
+}
