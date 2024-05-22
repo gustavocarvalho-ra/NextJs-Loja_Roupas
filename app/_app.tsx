41e531a2979b958e07298be6ef1363d46610593b
GustavoCR021 <gustavoc1922@gmail.com>
2024-05-21 23:13:42 -0300

    at _app.tsx

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
-import { CartProvider } from "../contexts/CartContext"
+import { CartProvider } from "./contexts/CartContext"
+import "../globals.css"
 import "normalize.css";
 
 export default function App({ Component, pageProps }: AppProps) {
