d6ced79fc5f98e2ad72a7fdc4b087d16bd510582
GustavoCR021 <gustavoc1922@gmail.com>
2024-05-30 00:02:50 -0300

    style: 404 end

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function NotFound() {
   return (
     <>
       <main style={{background: "black", display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: '100vw', height: '100vh'}}>
         <div>
-          <Image src={"/not.png"} alt="Image 404" width={500} height={500} />
+          <Image src={"/not.png"} alt="Image 404" width={650} height={650} />
         </div>
-        <h1>Ops, página não encontradaqqqqqqqqqq</h1>
-
-
+        <Link href={'/'} style={{background: '#a9ddef', fontSize: '30px', textDecoration: 'none', color: '#000', borderRadius: '8px'
+        }}>Voltar para o menu principal</Link>
       </main>
     
     </>
