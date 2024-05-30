906416ba47ea9ab7335c9af16dbb333fbcfac1e1
GustavoCR021 <gustavoc1922@gmail.com>
2024-05-29 23:44:56 -0300

    style: page 404 mod

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,10 +3,14 @@ import Image from "next/image";
 export default function NotFound() {
   return (
     <>
-      <div>
-        <Image src="/public/not.png" alt="Image 404" width={500} height={500} />
-      </div>
-      <h1>Ops, página não encontradaqqqqqqqqqq</h1>
+      <main style={{background: "black", display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: '100vw', height: '100vh'}}>
+        <div>
+          <Image src={"/not.png"} alt="Image 404" width={500} height={500} />
+        </div>
+        <h1>Ops, página não encontradaqqqqqqqqqq</h1>
+
+
+      </main>
     
     </>
     
