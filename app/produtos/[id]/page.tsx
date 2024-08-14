aea429152e887c3b15f7b28112561632a3d36f39
GustavoCR021 <gustavoc1922@gmail.com>
2024-08-14 14:49:47 -0300

    feat: test router products

---
+++ app/produtos/[id]/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Header from "@/app/components/Header";
+
+export default function Id() {
+
+  return (
+    <>
+      <Header/>
+      <h1>test</h1>
+    </>
+  );
+}
--- app/produtos/all/page.tsx
+++ app/produtos/all/page.tsx
@@ -1,10 +1,13 @@
 "use client"
 
 import pro from "../../data/pro.json"
-import Header from "@/app/components/Header"
 import Menu from "../../components/Menu";
+import Header from "@/app/components/Header"
 
+import Link from "next/link";
 import Image from "next/image";
+import { useParams } from "next/navigation";
+
 import { Al } from "./styles";
 
 
@@ -27,19 +30,21 @@ export default function Test () {
           .sort(() => Math.random() - 0.5)
           .map(item => (
             // eslint-disable-next-line react/jsx-key
-            <div className="card">
-                <button className="btd">20% off</button>
-              <div className="ft">
-                <Image src={item.photo}  alt="test" sizes="100vh" width={180} height={210} style={{borderRadius: "8px", marginTop: "4px"}}/>
-              </div>
-              <div className="info">
-                <h1 key={item.name}>{item.name}</h1>
-              <div className="nis" style={{display: "flex", justifyContent: "space-between"}}>
-                  <p key={item.price}>R$ {item.price}</p>
-                  <span className="var" key={item.des}>R$ {Math.floor(item.price * item.des + item.price)}</span>
+            <Link href={"./${item.id}"}>
+              <div className="card">
+                  <button className="btd">20% off</button>
+                <div className="ft">
+                  <Image src={item.photo}  alt="test" sizes="100vh" width={180} height={210} style={{borderRadius: "8px", marginTop: "4px"}}/>
+                </div>
+                <div className="info">
+                  <h1 key={item.name}>{item.name}</h1>
+                <div className="nis" style={{display: "flex", justifyContent: "space-between"}}>
+                    <p key={item.price}>R$ {item.price}</p>
+                    <span className="var" key={item.des}>R$ {Math.floor(item.price * item.des + item.price)}</span>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </Al>
