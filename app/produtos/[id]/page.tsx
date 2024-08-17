a6f21f53790ec690b3a5f3e8b094d1af8dbed6f8
GustavoCR021 <gustavoc1922@gmail.com>
2024-08-16 23:12:20 -0300

    feat: test search router

--- app/produtos/[id]/page.tsx
+++ app/produtos/[id]/page.tsx
@@ -1,21 +1,26 @@
 "use client"
 
 import Header from "@/app/components/Header";
-import { useParams } from "next/navigation";
+import { useParams, useSearchParams } from "next/navigation";
 import pro from "../../data/pro.json"
 
-export default function Detalhes() {
+export default function Detalhes( {params}: any) {
 
-  const params = useParams();
+  const searchParams = useSearchParams();
+  const newParam = searchParams.get("item")
 
-  console.log(params)
+  console.log(newParam)
+
+  // const params = useParams();
+
+  // console.log(params)
 
   let produtos = [...pro];
 
   return (
     <>
       <Header/>
-      {/* <h1>test produtosss ##{item.id}</h1> */}
+      <h1>test link ##{newParam}</h1>
     </>
   );
 }
