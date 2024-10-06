42cb761d6893233504636281bae0d2916adcedc3
GustavoCR021 <gustavoc1922@gmail.com>
2024-10-05 23:41:02 -0300

    feat: disable page search

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,31 +1,54 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-
-const fetchProducts = async (q: string) => {
-  const response = await fetch("/app/data/pro.json");
-  const products = await response.json();
-  return products.filter((product: any) =>
-    product.name.toLowerCase().includes(q.toLowerCase())
-  );
-};
-
-const SearchPage = () => {
-  const searchParams = useSearchParams();
-  const query = searchParams.get('q') || '';
-  const [products, setProducts] = useState<any[]>([]);
-
-  useEffect(() => {
-    const searchProducts = async () => {
-      const results = await fetchProducts(query);
-      setProducts(results);
-    };
-
-    if (query) {
-      searchProducts();
-    }
-  }, [query]);
-
-  return 
+export default function TestSearch() {
+  return(
+    <h1>test search</h1>
+  )
 }
+
+// import { useSearchParams } from "next/navigation";
+// import { useEffect, useState } from "react";
+
+// const fetchProducts = async (q: string) => {
+//   const response = await fetch("/app/data/pro.json");
+//   const products = await response.json();
+//   return products.filter((product: any) =>
+//     product.name.toLowerCase().includes(q.toLowerCase())
+//   );
+// };
+
+// const SearchPage = () => {
+//   const searchParams = useSearchParams();
+//   const query = searchParams.get('q') || '';
+//   const [products, setProducts] = useState<any[]>([]);
+
+//   useEffect(() => {
+//     const searchProducts = async () => {
+//       const results = await fetchProducts(query);
+//       setProducts(results);
+//     };
+
+//     if (query) {
+//       searchProducts();
+//     }
+//   }, [query]);
+
+//   return (
+//     <div>
+//       <h1>Resultados da busca: {query}</h1>
+//       {products.length > 0 ? (
+//         <ul>
+//           {products.map((product) => (
+//             <li key={product.id}>
+//               {product.name} - R$ {product.price}
+//             </li>
+//           ))}
+//         </ul>
+//       ) : (
+//         <p>Nenhum produto encontrado.</p>
+//       )}
+//     </div>
+//   )
+// }
+
+// export default SearchPage;
