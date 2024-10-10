72f95edbd95392f6d70efe4fec480782aff60f78
GustavoCR021 <gustavoc1922@gmail.com>
2024-10-09 22:02:02 -0300

    feat: test conteiner

--- app/components/ItensCard/index.tsx
+++ app/components/ItensCard/index.tsx
@@ -1,2 +1,39 @@
 import { Conte } from "./styles";
 
+"use client"
+
+import pro from "../../data/pro.json"
+import Menu from "../../components/Menu";
+import Header from "@/app/components/Header"
+
+import Link from "next/link";
+import Image from "next/image";
+
+
+export default function Test () {
+  let produtos = [...pro];
+
+  return (
+      <Conte>
+          {produtos
+          .sort(() => Math.random() - 0.5)
+          .map(item => (
+            <Link href={`/produtos/${item.id}`}>
+              <div className="card">
+                  <button className="btd">20% off</button>
+                <div className="ft">
+                  <Image src={item.photo}  alt="test" sizes="100vh" width={180} height={210} style={{borderRadius: "8px", marginTop: "4px"}}/>
+                </div>
+                <div className="info">
+                  <h1 key={item.name}>{item.name}</h1>
+                <div className="nis" style={{display: "flex", justifyContent: "space-between"}}>
+                    <p key={item.price}>R$ {item.price}</p>
+                    <span className="var" key={item.des}>R$ {Math.floor(item.price * item.des + item.price)}</span>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))}
+      </Conte>
+  )
+}
