c801f3e6757ad735b9477105fe56885c269d41e4
GustavoCR021 <gustavoc1922@gmail.com>
2024-10-18 20:45:37 -0300

    feat: style component deleted

--- app/produtos/styles.ts
+++ app/produtos/styles.ts
@@ -17,46 +17,6 @@ export const Body = styled.div `
   flex-direction: column;
 `
 
-export const Card = styled.div `
-  .card {
-    width: 210px;
-    height: 310px;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    transition: 1s;
-    border-radius: 8px;
-    background-color: var(--card);
-    padding-top: .3em;
-    cursor: pointer;
-
-    &:hover {
-      scale: 1.01;
-      box-shadow: 6px 8px 8px 0px #1a0f27;
-    }
-
-    .info {
-      margin: 10px;
-      width: 90%;
-      display: flex;
-      flex-direction: column;
-
-      .nis {
-        margin-top: 8px;
-        display: flex;
-
-        span {
-          font-size: 13px;
-          text-decoration: line-through;
-          color: var(--grey);
-          font-weight: 600;
-          margin-right: 3em;
-        }
-      }
-    }
-  }
-`
-
 export const Main = styled.div `
   max-width: 100vw;
   height: 120vh;
