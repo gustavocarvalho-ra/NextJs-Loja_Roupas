05261539806302f71a4480f5fd5252828b551a19
GustavoCR021 <gustavoc1922@gmail.com>
2024-11-02 22:46:23 -0300

    style: ajust position and height

--- app/components/CartItens/styles.ts
+++ app/components/CartItens/styles.ts
@@ -3,13 +3,15 @@ import styled from "styled-components";
 export const CartI = styled.div `
   position: absolute;
   right: 0;
-  top: 17.8vh;
+  top: 17vh;
   width: 20vw;
-  height: 70vh;
+  height: 82vh;
   display: flex;
   align-content: center;
   justify-content: center;
   z-index: 20;
   background: linear-gradient(29deg, rgba(142,0,167,0.7) 20%, rgba(0,206,235,1) 100%);
-  border: 2px solid var(--grey);
+  /* border: 2px solid var(--grey); */
+  box-shadow: -2px 1px 5px var(--purple);
+  position: fixed;
 `
