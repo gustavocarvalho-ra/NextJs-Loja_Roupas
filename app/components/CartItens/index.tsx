fc73c00dc21c2bb1f6691cbdc96aa1d8d750b4f7
GustavoCR021 <gustavoc1922@gmail.com>
2024-11-07 10:45:37 -0300

    feat: shopping cart test

--- app/components/CartItens/index.tsx
+++ app/components/CartItens/index.tsx
@@ -1,4 +1,7 @@
+import { useState, useEffect } from "react";
+
 import { CartI } from "./styles";
+import { products } from "@/app/types/products";
 
 interface ModalProps {
   isOpen?: boolean;
@@ -9,17 +12,31 @@ export default function CartItens({isOpen, onClose}: ModalProps) {
   
   if (!isOpen) return null;
 
+  const [items, setItems] = useState<products[]>([]);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedItems = localStorage.getItem("listShoppingCartStorage");
+
+      if (storedItems) {
+        setItems(JSON.parse(storedItems));
+      }
+    }
+  }, [])
+
   return (
 
     <CartI>
       <div className="image" style={{pointerEvents: "none"}}/>
 
-      {/* listShoppingCart.map((item) => (
-        <div key={item.id}>
-          <p>{item.nome}</p>
-          <p>Quantidade: {item.quantidade}</p>
-        </div> */}
-      {/* )); */}
+      <div className="container">
+        {items.map((item, index) => (
+          <div key={index} className="card">
+            <h2>{item.name}</h2>
+            <h3>{item.price}</h3>
+          </div>
+        ))}
+      </div>
     </CartI>
   )
 }
