552ebcb8e6655fb61926622c06951ff86f0172aa
GustavoCR021 <gustavoc1922@gmail.com>
2024-11-12 17:24:18 -0300

    feat: adding message on empty cart

--- app/components/CartItens/index.tsx
+++ app/components/CartItens/index.tsx
@@ -32,43 +32,47 @@ export default function CartItens({isOpen, onClose}: ModalProps) {
   return (
 
     <CartI>
-      {listShoppingCart.length > 0 && (
-        <div className="totalCart">
-          <h2>Total: </h2>
-          <h2>R${total.toFixed(2)}</h2>
+      {listShoppingCart.length > 0 ? (
+        <>
+          <div className="totalCart">
+            <h2>Total: </h2>
+            <h2>R${total.toFixed(2)}</h2>
+          </div>
+        <div className="conadding message on empty carttainer">
+          {listShoppingCart.map((item) => (
+            <div key={item.id} className="card">
+              <div className="image">
+                <Image className="img" src={item.photo} alt={item.name} width={60} height={60}/>
+              </div>
+
+              <div className="description">
+                <h4>{item.name}</h4>
+                <h5>R$ {item.price}</h5>
+              </div>
+
+              <div className="btn">
+                <button onClick={() => handleDecreaseQuantity(item.id)}>-</button>
+                <p>{item.quantity}</p>
+                <button onClick={() => handleIncreaseQuantity(item.id)}>+</button>
+              </div>
+
+              <div className="close">
+                <button onClick={() => handleRemoveProductShopping(item)}><FaTrash /></button>
+              </div>
+            </div>
+          ))}
+            <div className="cash">
+              <Link href={"../cart"}>
+                <h2>Finalizar compra</h2>
+              </Link>
+            </div>
+        </div>
+        </>
+      ) : (
+        <div className="emptyCart">
+          <h2>O carrinho está vazio</h2>
         </div>
       )}
-      <div className="container">
-        {listShoppingCart.map((item) => (
-          <div key={item.id} className="card">
-            <div className="image">
-              <Image className="img" src={item.photo} alt={item.name} width={60} height={60}/>
-            </div>
-
-            <div className="description">
-              <h4>{item.name}</h4>
-              <h5>R$ {item.price}</h5>
-            </div>
-
-            <div className="btn">
-              <button onClick={() => handleDecreaseQuantity(item.id)}>-</button>
-              <p>{item.quantity}</p>
-              <button onClick={() => handleIncreaseQuantity(item.id)}>+</button>
-            </div>
-
-            <div className="close">
-              <button onClick={() => handleRemoveProductShopping(item)}><FaTrash /></button>
-            </div>
-          </div>
-        ))}
-        {listShoppingCart.length > 0 && (
-          <div className="cash">
-            <Link href={"../cart"}>
-              <h2>Finalizar compra</h2>
-            </Link>
-          </div>
-        )}
-      </div>
     </CartI>
   )
 }
