4f9d9584e374b9d8012138ab78078dd8f3443dd5
GustavoCR021 <gustavoc1922@gmail.com>
2024-11-13 22:57:38 -0300

    feat: add image for cart

--- app/components/CartItens/index.tsx
+++ app/components/CartItens/index.tsx
@@ -71,6 +71,9 @@ export default function CartItens({isOpen, onClose}: ModalProps) {
       ) : (
         <div className="emptyCart">
           <h2>O carrinho está vazio</h2>
+          <div>
+            <Image src={"/empty-cart.png"} alt="Imagem do carrinho" width={100} height={100}/>
+          </div>
         </div>
       )}
     </CartI>
