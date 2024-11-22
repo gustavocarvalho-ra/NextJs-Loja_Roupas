af106eaeb43c22256bc0b793d12e419bf8917f3e
GustavoCR021 <gustavoc1922@gmail.com>
2024-11-22 00:12:02 -0300

    feat: change card for component

--- app/produtos/all/page.tsx
+++ app/produtos/all/page.tsx
@@ -1,17 +1,15 @@
 "use client"
 
-import pro from "../../data/pro.json"
-import Menu from "../../components/Menu";
-import Header from "@/app/components/Header"
-
-import Link from "next/link";
-import Image from "next/image";
-
 import { Container } from "../styles";
 
+import Header from "@/app/components/Header"
+import Menu from "../../components/Menu";
+import ProductCard from "@/app/components/ProductCard/ProductCard";
+
+import pro from "../../data/pro.json"
 
 export default function Test () {
-  let produtos = [...pro];
+  const produtos = [...pro].sort(() => Math.random() - 0.5);
 
   return (
     <>
@@ -25,24 +23,15 @@ export default function Test () {
             <h1 className="ti">Todos os produtos</h1>
           </div>
 
-          {produtos
-          .sort(() => Math.random() - 0.5)
-          .map(item => (
-            <Link href={`/produtos/${item.id}`}>
-              <div className="card">
-                  <button className="btd">20% off</button>
-                <div className="ft">
-                  <Image src={item.photo}  alt="test" sizes="100vh" width={180} height={210} style={{borderRadius: "8px", marginTop: "4px"}}/>
-                </div>
-                <div className="info">
-                  <h1 key={item.name}>{item.name}</h1>
-                <div className="nis" style={{display: "flex", justifyContent: "space-between"}}>
-                    <p key={item.price}>R$ {item.price}</p>
-                    <span className="var" key={item.des}>R$ {Math.floor(item.price * item.des + item.price)}</span>
-                  </div>
-                </div>
-              </div>
-            </Link>
+          {produtos.map((item) => (
+            <ProductCard
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              photo={item.photo}
+              price={item.price}
+              des={item.des}
+            />
           ))}
         </div>
       </Container>
