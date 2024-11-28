4431069fa5f4bd33c64f45f095c3181af299c303
GustavoCR021 <gustavoc1922@gmail.com>
2024-11-27 23:32:10 -0300

    style: change to variable color

--- app/produtos/styles.ts
+++ app/produtos/styles.ts
@@ -155,8 +155,8 @@ export const Main = styled.div `
     top: -100px;
     left: 50%;
     transform: translateX(-50%);
-    background-color: #4caf50;
-    color: white;
+    background-color: var(--warn);
+    color: var(--tex);
     padding: 10px 20px;
     border-radius: 8px;
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
