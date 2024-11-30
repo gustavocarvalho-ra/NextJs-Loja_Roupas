725d91fe62d02b48be4d1f1709210c15d91413b4
GustavoCR021 <gustavoc1922@gmail.com>
2024-11-29 23:56:56 -0300

    feat: folder organization

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import pro from "../data/pro.json"
+import pro from "../../data/pro.json"
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
