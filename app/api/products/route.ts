53f7169ddc1bf0bd012a27d240286ec01f5ab7e4
GustavoCR021 <gustavoc1922@gmail.com>
2024-11-30 01:04:00 -0300

    feat: finish route

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,24 +1,24 @@
 import { NextResponse } from "next/server";
-import pro from "../../data/pro.json"
+import pro from "../../data/pro.json";
 
 function normalizeString(str: string): string {
-  return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
 }
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
-  const query = searchParams.get('q') ?? '';
+  const query = searchParams.get("q") ?? "";
 
   if (!query) {
-    return NextResponse.json([])
+    return NextResponse.json([]);
   }
 
   const normalizedQuery = normalizeString(query);
 
   const filteredPro = pro.filter(
     (item) =>
-      normalizedQuery(item.name).includes(normalizedQuery) ||
-      normalizedQuery(item.type).includes(normalizedQuery)
+      normalizeString(item.name).includes(normalizedQuery) ||
+      normalizeString(item.type).includes(normalizedQuery)
   );
 
   return NextResponse.json(filteredPro);
