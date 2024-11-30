49aaaaab9ec4e5f69f635d921b23da6927246621
GustavoCR021 <gustavoc1922@gmail.com>
2024-11-30 01:02:30 -0300

    feat: test

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,18 +1,24 @@
 import { NextResponse } from "next/server";
 import pro from "../../data/pro.json"
 
+function normalizeString(str: string): string {
+  return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+}
+
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
-  const query = searchParams.get('q')?.toLowerCase();
+  const query = searchParams.get('q') ?? '';
 
   if (!query) {
-    return NextResponse.json(pro)
+    return NextResponse.json([])
   }
 
-  const filteredPro = pro.filter (
+  const normalizedQuery = normalizeString(query);
+
+  const filteredPro = pro.filter(
     (item) =>
-      item.name.toLowerCase().includes(query) ||
-      item.type.toLowerCase().includes(query)
+      normalizedQuery(item.name).includes(normalizedQuery) ||
+      normalizedQuery(item.type).includes(normalizedQuery)
   );
 
   return NextResponse.json(filteredPro);
