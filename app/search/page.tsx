6352a2ee3a48ab5732d1e538c2ae1d5b57f78909
GustavoCR021 <gustavoc1922@gmail.com>
2024-11-29 22:47:07 -0300

    feat: test search

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,54 +1,52 @@
 "use client";
 
-export default function TestSearch() {
+export default function Search() {
   return(
     <h1>test search</h1>
   )
 }
 
-// import { useSearchParams } from "next/navigation";
-// import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
-// const fetchProducts = async (q: string) => {
-//   const response = await fetch("/app/data/pro.json");
-//   const products = await response.json();
-//   return products.filter((product: any) =>
-//     product.name.toLowerCase().includes(q.toLowerCase())
-//   );
-// };
+const fetchProducts = async (q: string) => {
+  const response = await fetch("/app/data/pro.json");
+  const products = await response.json();
+  return products.filter((product: any) =>
+    product.name.toLowerCase().includes(q.toLowerCase())
+  );
+};
 
-// const SearchPage = () => {
-//   const searchParams = useSearchParams();
-//   const query = searchParams.get('q') || '';
-//   const [products, setProducts] = useState<any[]>([]);
+const SearchPage = () => {
+  const searchParams = useSearchParams();
+  const query = searchParams.get('q') || '';
+  const [products, setProducts] = useState<any[]>([]);
 
-//   useEffect(() => {
-//     const searchProducts = async () => {
-//       const results = await fetchProducts(query);
-//       setProducts(results);
-//     };
+  useEffect(() => {
+    const searchProducts = async () => {
+      const results = await fetchProducts(query);
+      setProducts(results);
+    };
 
-//     if (query) {
-//       searchProducts();
-//     }
-//   }, [query]);
+    if (query) {
+      searchProducts();
+    }
+  }, [query]);
 
-//   return (
-//     <div>
-//       <h1>Resultados da busca: {query}</h1>
-//       {products.length > 0 ? (
-//         <ul>
-//           {products.map((product) => (
-//             <li key={product.id}>
-//               {product.name} - R$ {product.price}
-//             </li>
-//           ))}
-//         </ul>
-//       ) : (
-//         <p>Nenhum produto encontrado.</p>
-//       )}
-//     </div>
-//   )
-// }
-
-// export default SearchPage;
+  return (
+    <div>
+      <h1>Resultados da busca: {query}</h1>
+      {products.length > 0 ? (
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$ {product.price}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Nenhum produto encontrado.</p>
+      )}
+    </div>
+  )
+}
