0316d5399775e59b2febaab38ccbd9b449065df9
GustavoCR021 <gustavoc1922@gmail.com>
2024-12-27 21:35:48 -0300

    style: ajust identation

--- app/components/ItensCard/styles.ts
+++ app/components/ItensCard/styles.ts
@@ -71,54 +71,53 @@ export const Conte = styled.div `
         }
       }
 
-        .card {
-          width: 210px;
-          height: 310px;
+      .card {
+        width: 210px;
+        height: 310px;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        transition: 1s;
+        border-radius: 8px;
+        background-color: #f0f4f5;
+        padding-top: .3em;
+        cursor: pointer;
+
+        &:hover {
+          scale: 1.01;
+          box-shadow: 6px 8px 8px 0px #1a0f27;
+        }
+
+        .info {
+          margin: 10px;
+          width: 90%;
           display: flex;
-          align-items: center;
           flex-direction: column;
-          transition: 1s;
-          border-radius: 8px;
-          background-color: #f0f4f5;
-          padding-top: .3em;
-          cursor: pointer;
-
-          &:hover {
-            scale: 1.01;
-            box-shadow: 6px 8px 8px 0px #1a0f27;
-          }
-
-          .info {
-            margin: 10px;
-            width: 90%;
+    
+          .nis {
+            margin-top: 8px;
             display: flex;
-            flex-direction: column;
-      
-            .nis {
-              margin-top: 8px;
-              display: flex;
-      
-              span {
-                font-size: 13px;
-                text-decoration: line-through;
-                color: var(--grey);
-                font-weight: 600;
-                margin-right: 3em;
-              }
+    
+            span {
+              font-size: 13px;
+              text-decoration: line-through;
+              color: var(--grey);
+              font-weight: 600;
+              margin-right: 3em;
             }
           }
-          
-          h1{
-            font-size: 14px;
-            font-weight: 400;
-          } 
-          
-          p {
-            margin-top: 5px;
-            font-size: 17px;
-            color: var(--green);
-            font-weight: 600;
-          }
+        }
+        
+        h1{
+          font-size: 14px;
+          font-weight: 400;
+        } 
+        
+        p {
+          margin-top: 5px;
+          font-size: 17px;
+          color: var(--green);
+          font-weight: 600;
         }
       }
     }
